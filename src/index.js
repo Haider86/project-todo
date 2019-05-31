d9895b9e71d2587421116f368dcd133ca3234393
haider <cbm_cbm2@yahoo.com>
2019-05-31 12:24:09 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+
+
+/*class Car extends Component {
+    render() {
+      return <h2>Hi, I am a Car!</h2>;
+    }
+  }
+  
+  ReactDOM.render(<Car />, document.getElementById('root'));
+  */
+
+  class Car extends Component{
+      render(){
+          return <h2>hufdh</h2>;
+      }
+  }
+
+
+  ReactDOM.render(<Car />, document.getElementById('root'))
+
+  
